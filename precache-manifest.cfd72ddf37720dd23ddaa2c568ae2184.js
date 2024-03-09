7c1021096d3b9d8a07e4d6b382acfc2d61c0d9e8
Ankur2903 <112171452+Ankur2903@users.noreply.github.com>
2024-03-09 12:59:52 +0530

    initial files

---
+++ precache-manifest.cfd72ddf37720dd23ddaa2c568ae2184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d677979ac55681caa44d20a886b6a755",
+    "url": "/React-app/index.html"
+  },
+  {
+    "revision": "e0b077fa3786e4b4ed5d",
+    "url": "/React-app/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "a1ab09171680eded89b2",
+    "url": "/React-app/static/js/2.25303d78.chunk.js"
+  },
+  {
+    "revision": "60c46b200b0cfd41617f",
+    "url": "/React-app/static/js/3.b265e9c7.chunk.js"
+  },
+  {
+    "revision": "e0b077fa3786e4b4ed5d",
+    "url": "/React-app/static/js/main.135f9c70.chunk.js"
+  },
+  {
+    "revision": "04964201650395a285ef",
+    "url": "/React-app/static/js/runtime~main.b48a1835.js"
+  }
+]);
